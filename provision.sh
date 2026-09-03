ce15782e3e5f62000728b12a3260cc640e959640
DaviPO2001 <davi9514@outlook.com>
2026-09-02 22:06:22 -0300

    README

--- provision.sh
+++ provision.sh
@@ -6,8 +6,11 @@ apt update
 echo "Instalando Ansible..."
 apt install -y ansible
 
+#echo "Iniciando o playbook de automação..."
 
+#cd /vagrant/
 
+#ansible-playbook -i ansible/inventory.ini ansible/playbook.yml
 
 
 
